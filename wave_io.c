5e685d3c135454c6e816f046906bcd4ff833967d
Tim Jagodzinski <tim.jagodzinski@mtc-berlin.com>
2012-10-19 14:12:18 +0200

    adds exercises 3.1 - 3.3

--- wave_io.c
+++ wave_io.c
@@ -11,44 +11,77 @@
 #define PI 3.141592653
 
 int main(int argc, char *argv[])
-  {
-  int	out;
-      
-  char in_name[MAX_STR_LNGTH], out_name[MAX_STR_LNGTH];
-  short *wave;
+{
+	int	out;
 
-  unsigned int freq_in, bits_in;
-  unsigned int n_wave;
-   
-  struct Waveheader header;
+	char in_name[MAX_STR_LNGTH], out_name[MAX_STR_LNGTH];
+	short *wave;
+
+	unsigned int freq_in, bits_in;
+	unsigned int n_wave;
+	unsigned int i;
+	FILE *prt; 
+
+	struct Waveheader header;
 
 
-  if ( (argc != 3) && (argc != 5) )
-    {
-    fprintf(stderr, "Usage: %s -i infile [-o outfile]\n", argv[0]);
-    exit(1);
-    }
-  else
-    {
-    strcpy(in_name, argv[2]);
-    out=0;
-    out_name[0]='\0';
+	if ( (argc != 3) && (argc != 5) )
+	{
+		fprintf(stderr, "Usage: %s -i infile [-o outfile]\n", argv[0]);
+		exit(1);
+	}
+	else
+	{
+		strcpy(in_name, argv[2]);
+		out=0;
+		out_name[0]='\0';
 
-    if (argc == 5)
-      {
-      strcpy(out_name, argv[4]);
-      out=1;
-      }
-    }
-  
-  fprintf(stderr,"infile: %s outfile: %s\n", in_name, out_name);
+		if (argc == 5)
+		{
+			strcpy(out_name, argv[4]);
+			out=1;
+		}
+	}
 
-  read_wave(&wave, &n_wave, &freq_in, &bits_in, in_name, &header);
-  if (out)
-    write_wave(wave, n_wave, freq_in, bits_in, out_name, &header);
+	fprintf(stderr,"infile: %s outfile: %s\n", in_name, out_name);
 
-  free(wave);
-  
-  return(1);
-  }
+
+	read_wave(&wave, &n_wave, &freq_in, &bits_in, in_name, &header);
+
+	// --------- 2.1 output curve to ascii file --------------
+	
+	/*prt = fopen ("prt.txt","wt"); 
+
+	for(i=0 ; i<n_wave;i++){ 
+		fprintf(prt,"%i \n",wave[i]); 
+	} 
+	fclose(prt); */
+
+	// -------- 2.1 output curve to ascii file end ----------
+
+	// -------------- 2.4 downsampling ----------------------
+	
+	/*for (i=0; i < n_wave / 2; i++){
+		wave[i] = wave[i*2];
+	}
+	freq_in /= 2;
+	n_wave /= 2;*/
+
+	// -------------- 2.4 downsampling end ------------------
+
+	// --------------3.2 bit reduction ----------------------
+
+	for (i =0; i < n_wave; i++){
+		wave[i] /= 512;
+		wave[i] *= 512;
+	}
+	// -------------- 3.2 bit reduction end -----------------
+
+	if (out)
+		write_wave(wave, n_wave, freq_in, bits_in, out_name, &header);
+
+	free(wave);
+
+	return(1);
+}
 
